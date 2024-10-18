804f7c4618386346d5d7f29323e1fa8fd7589c9e
Alfonso Magallanes <A.Magallanes@edemsa1.local>
2024-10-18 12:30:28 -0300

    orse

--- src/Components/Configuracion/Configuracion.jsx
+++ src/Components/Configuracion/Configuracion.jsx
@@ -6,39 +6,97 @@ import { useNavigate } from 'react-router-dom';
 const Configuracion = () => {
   const navigate = useNavigate();
   const [permisos, setPermisos] = useState(null);
+  const [usuarios, setUsuarios] = useState([]);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [selectedUserPermissions, setSelectedUserPermissions] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedUserNick, setSelectedUserNick] = useState(null);
 
   useEffect(() => {
-    // Verificar el localStorage al montar el componente
     if (!(localStorage.getItem('me') > 0)) {
       navigate('/');
     }
   }, [navigate]);
 
   useEffect(() => {
-    const fetchPermisos = async () => {
-      try {
-        const nick = localStorage.getItem('me');
-        if (!nick) {
-          console.error('No se encontró el nick en el localStorage');
-          return;
-        }
-
-        const response = await axios.get('http://localhost:8080/getpermisosbynick', {
-          params: { NICK: nick },
-        });
-
-        if (response.data) {
-          setPermisos(response.data.result);
-        } else {
-          console.error('No se encontraron permisos con ese nick');
-          setPermisos(null);
-        }
-      } catch (error) {
-        console.error('Error al obtener permisos:', error);
-      }
-    };
     fetchPermisos();
   }, []);
+
+  const fetchPermisos = async () => {
+    try {
+      const nick = localStorage.getItem('me');
+      if (!nick) {
+        console.error('No se encontró el nick en el localStorage');
+        return;
+      }
+
+      const response = await axios.get('http://localhost:8080/getpermisosbynick', {
+        params: { NICK: nick },
+      });
+
+      if (response.data) {
+        setPermisos(response.data.result);
+
+        if (nick === '2000826') {
+          setIsAdmin(true);
+          fetchUsuarios();
+        }
+      } else {
+        console.error('No se encontraron permisos con ese nick');
+        setPermisos(null);
+      }
+    } catch (error) {
+      console.error('Error al obtener permisos:', error);
+    }
+  };
+
+  const fetchUsuarios = async () => {
+    try {
+      const sucursal = localStorage.getItem('sucursal');
+      if (!sucursal) {
+        console.error('No se encontró la sucursal en el localStorage');
+        return;
+      }
+
+      const response = await axios.get('http://localhost:8080/getUsuariosbyCod', {
+        params: { codUnicom: sucursal },
+      });
+
+      if (response.data && Array.isArray(response.data.result)) {
+        setUsuarios(response.data.result);
+      } else {
+        console.error('No se encontraron usuarios o el formato es incorrecto');
+        setUsuarios([]);
+      }
+    } catch (error) {
+      console.error('Error fetching usuarios:', error);
+      setUsuarios([]);
+    }
+  };
+
+
+  const openPermissionsModal = async (usuario) => {
+    try {
+      const response = await axios.get('http://localhost:8080/getpermisosbynick', {
+        params: { NICK: usuario.LEGAJO },
+      });
+
+      if (response.data) {
+        setSelectedUserPermissions(response.data.result);
+        setSelectedUserNick(usuario.LEGAJO);
+        setIsModalOpen(true);
+        console.log(response.data.result);
+      } else {
+        console.error('No se encontraron permisos para el usuario seleccionado');
+      }
+    } catch (error) {
+      console.error('Error al obtener permisos del usuario:', error);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className="configuracion-page">
@@ -54,7 +112,6 @@ const Configuracion = () => {
         </header>
 
         <div className="configuracion-content">
-          {/* Sección de permisos */}
           {permisos ? (
             <div className="permisos-section">
               <h2>Permisos - Turnero</h2>
@@ -71,6 +128,43 @@ const Configuracion = () => {
             </div>
           ) : (
             <p>Cargando permisos...</p>
+          )}
+
+          {isAdmin && (
+            <div className="admin-section">
+              <h2>Vista del Administrador</h2>
+              <ul>
+                {usuarios.map((usuario) => (
+                  <li key={usuario.LEGAJO}>
+                    {usuario.NOMBRECOMPLETO}
+                    <button className="cambiar-permisos-button" onClick={() => openPermissionsModal(usuario)}>Ver permisos</button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          {isModalOpen && (
+            <div className="modal-overlay" onClick={handleCloseModal}>
+              <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <h2>Permisos del Usuario</h2>
+                {selectedUserPermissions ? (
+                  <ul>
+                    <li>Añadir boxes: {selectedUserPermissions.turnero.add_boxes ? 'Si' : 'No'}</li>
+                    <li>Añadir motivos de visita: {selectedUserPermissions.turnero.add_motivosvisita ? 'Si' : 'No'}</li>
+                    <li>Administrar usuarios en boxes: {selectedUserPermissions.turnero.admin_usuarios_x_box ? 'Si' : 'No'}</li>
+                    <li>Eliminar boxes: {selectedUserPermissions.turnero.del_boxes ? 'Si' : 'No'}</li>
+                    <li>Eliminar motivos de visita: {selectedUserPermissions.turnero.del_motivosvisita ? 'Si' : 'No'}</li>
+                    <li>Llamar turno: {selectedUserPermissions.turnero.llamar_turno ? 'Si' : 'No'}</li>
+                    <li>Ver motivos de visita: {selectedUserPermissions.turnero.ver_motivosvisita ? 'Si' : 'No'}</li>
+                    <li>Ver turnos: {selectedUserPermissions.turnero.ver_turnos ? 'Si' : 'No'}</li>
+                  </ul>
+                ) : (
+                  <p>No se encontraron permisos para este usuario.</p>
+                )}
+                <button className="cerrar-button" onClick={handleCloseModal}>Cerrar</button>
+              </div>
+            </div>
           )}
         </div>
       </main>
