f809a9e139b0683a6eb4a1d34c379863ae08e2c7
juanma <juanma@edemsa.com>
2024-10-21 14:37:20 -0300

    delet  boxes

--- src/Components/CajaEmpleados/CajaEmpleados.jsx
+++ src/Components/CajaEmpleados/CajaEmpleados.jsx
@@ -4,21 +4,20 @@ import './CajaEmpleados.css';
 import { useNavigate } from 'react-router-dom';
 
 const CajaEmpleados = () => {
-  const [cajas, setCajas] = useState([]); // Estado para almacenar las cajas
-  const [usuarios, setUsuarios] = useState([]); // Estado para almacenar los usuarios
-  const [selectedBoxes, setSelectedBoxes] = useState({}); // Para manejar las selecciones de empleados por caja
+  const [cajas, setCajas] = useState([]);
+  const [usuarios, setUsuarios] = useState([]);
+  const [selectedBoxes, setSelectedBoxes] = useState({});
+  const [newBoxName, setNewBoxName] = useState('');
+  const [showAddBoxForm, setShowAddBoxForm] = useState(false);
+  const [showDeleteButtons, setShowDeleteButtons] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Verificar el localStorage al montar el componente
     if (!(localStorage.getItem("me") > 0)) {
       navigate('/');
     }
   }, [navigate]);
 
-  
-
-  // Obtener las cajas según el COD_UNICOM desde el backend
   useEffect(() => {
     const fetchCajas = async () => {
       try {
@@ -33,21 +32,20 @@ const CajaEmpleados = () => {
         });
 
         if (response.data && Array.isArray(response.data.result)) {
-          setCajas(response.data.result); // Asignar las cajas al estado
+          setCajas(response.data.result);
         } else {
           console.error('No se encontraron cajas o el formato es incorrecto');
-          setCajas([]); // Asegúrate de manejar este caso
+          setCajas([]);
         }
       } catch (error) {
         console.error('Error fetching cajas:', error);
-        setCajas([]); // En caso de error, asegura que sea un array vacío
+        setCajas([]);
       }
     };
 
     fetchCajas();
   }, []);
 
-  // Obtener los usuarios desde el backend
   useEffect(() => {
     const fetchUsuarios = async () => {
       try {
@@ -58,39 +56,82 @@ const CajaEmpleados = () => {
         }
 
         const response = await axios.get('http://localhost:8080/getUsuariosbyCod', {
-          params: { codUnicom: sucursal }, // Usa el mismo parámetro
+          params: { codUnicom: sucursal },
         });
 
         if (response.data && Array.isArray(response.data.result)) {
-          setUsuarios(response.data.result); // Asignar los usuarios al estado
+          setUsuarios(response.data.result);
         } else {
           console.error('No se encontraron usuarios o el formato es incorrecto');
-          setUsuarios([]); // Asegúrate de manejar este caso
+          setUsuarios([]);
         }
       } catch (error) {
         console.error('Error fetching usuarios:', error);
-        setUsuarios([]); // En caso de error, asegura que sea un array vacío
+        setUsuarios([]);
       }
     };
 
     fetchUsuarios();
   }, []);
 
-  // Manejar el cambio en el dropdown de empleados
   const handleBoxChange = (cajaId, empleadoId) => {
     setSelectedBoxes((prevSelectedBoxes) => ({
       ...prevSelectedBoxes,
-      [cajaId]: empleadoId, // Guardar la selección de empleado para cada caja
+      [cajaId]: empleadoId,
     }));
   };
 
-  // Navegar a la página de turnos
   const handleTurnoClick = () => {
     navigate('/dashboard');
   };
 
   const handleConfigClick = () => {
     navigate('/config');
+  };
+
+  const handleAddBoxClick = () => {
+    setShowAddBoxForm(true);
+  };
+
+  const handleAddBoxSubmit = async () => {
+    try {
+      const sucursal = localStorage.getItem('sucursal');
+      const created_by = localStorage.getItem('me');
+
+      const response = await axios.post('http://localhost:8080/addbox', { // Asegúrate de que la ruta sea correcta
+        nombre_box: newBoxName,
+        COD_UNICOM: sucursal,
+        created_by
+      });
+      console.log('Respuesta de agregar caja:', response.data); // Agrega esto para inspeccionar la respuesta
+
+      if (response.data && response.data.success) {
+        setCajas([...cajas, response.data.newBox]); // Asegúrate de que la respuesta contenga la nueva caja
+        setNewBoxName('');
+        setShowAddBoxForm(false);
+      } else {
+        console.error('Error al agregar la caja.');
+      }
+    } catch (error) {
+      console.error('Error al agregar la caja:', error);
+    }
+  };
+
+  const handleDeleteBox = async (id) => {
+    try {
+      const response = await axios.delete(`http://localhost:8080/deletebox/${id}`); // Asegúrate de que la ruta sea correcta
+      if (response.data && response.data.success) {
+        setCajas(cajas.filter(caja => caja.id !== id));
+      } else {
+        console.error('Error al eliminar la caja.');
+      }
+    } catch (error) {
+      console.error('Error al eliminar la caja:', error);
+    }
+  };
+
+  const toggleDeleteButtons = () => {
+    setShowDeleteButtons(!showDeleteButtons);
   };
 
   return (
@@ -105,28 +146,44 @@ const CajaEmpleados = () => {
         <header className="header">
           <h1>Cajas</h1>
           <div className="button-container">
-            <button className="add-box-button">Agregar Caja</button>
-            <button className="delete-box-button">Eliminar Caja</button>
+            <button onClick={handleAddBoxClick} className="add-box-button">Agregar Caja</button>
+            <button onClick={toggleDeleteButtons} className="delete-box-button">
+              {showDeleteButtons ? 'Cancelar Eliminar' : 'Eliminar Caja'}
+            </button>
           </div>
         </header>
 
+        {showAddBoxForm && (
+          <div className="add-box-form">
+            <input
+              type="text"
+              value={newBoxName}
+              onChange={(e) => setNewBoxName(e.target.value)}
+              placeholder="Nombre de la nueva caja"
+            />
+            <button className="agregar" onClick={handleAddBoxSubmit}>Agregar</button>
+            <button className="cancelar" onClick={() => setShowAddBoxForm(false)}>Cancelar</button>
+          </div>
+        )}
+
         <div className="box">
-          {/* Mostrar las cajas */}
           {cajas.map((caja) => (
             <div key={caja.id} className="caja-item">
               <h2>{caja.nombre_box}</h2>
               <select
-                value={selectedBoxes[caja.id] || ''} // Selección actual para esta caja
+                value={selectedBoxes[caja.id] || ''}
                 onChange={(e) => handleBoxChange(caja.id, e.target.value)}
               >
                 <option value="">Seleccionar empleado</option>
-                {/* Agregar opciones de usuarios al dropdown */}
                 {usuarios.map((usuario) => (
                   <option key={usuario.LEGAJO} value={usuario.LEGAJO}>
-                    {usuario.NOMBRECOMPLETO} {/* Cambia 'nombre' por el atributo adecuado de usuario */}
+                    {usuario.NOMBRECOMPLETO}
                   </option>
                 ))}
               </select>
+              {showDeleteButtons && (
+                <button className="delete-box-button" onClick={() => handleDeleteBox(caja.id)}>Eliminar</button>
+              )}
             </div>
           ))}
         </div>
